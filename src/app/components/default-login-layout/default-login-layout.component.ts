628d0ccfa705694032fd0d969abb35023ef69dea
Paulo Marcelo <paulo.marceloo@outlook.com>
2026-03-24 14:48:34 -0300

    feat: create login page

--- src/app/components/default-login-layout/default-login-layout.component.ts
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter,  } from '@angular/core';
 import { ɵEmptyOutletComponent } from "@angular/router";
 
 @Component({
@@ -12,5 +12,17 @@ export class DefaultLoginLayoutComponent {
   @Input() title: string = "";
   @Input() primaryButtonText: string = "";
   @Input() secondaryButtonText: string = "";
+  @Input() disablePrimaryBtn: boolean = true;
+  @Output("submit") onSubmit = new EventEmitter();
+
+  @Output("navigate") onNavigate = new EventEmitter();
+
+  submit() {
+    this.onSubmit.emit();
+  }
+
+    navigate() {
+    this.onNavigate.emit();
+  }
 
 }
